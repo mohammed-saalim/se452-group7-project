4530aa51ca68a97f595579dbff24e99073cfbb1a
mohammed-saalim <mohammed.saalim.k@gmail.com>
2024-09-23 11:40:05 -0500

    orders module update

---
+++ src/main/java/edu/depaul/cdm/se452/groupProject/orders/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author: Mohammed Saalim Kartapillai
+ * All the features or functionality related to Orders and Cart module would be available in this package
+ */
+
+ package edu.depaul.cdm.se452.groupProject.orders;
