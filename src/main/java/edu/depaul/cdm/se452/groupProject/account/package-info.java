a53205c7b71c27c8a0a5956b5143d44fef2f3a4c
bramesh1 <bramesh1@depaul.edu>
2024-10-14 19:53:50 -0500

    Persistence code for Account and Customer

--- src/main/java/edu/depaul/cdm/se452/groupProject/account/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/groupProject/account/package-info.java
@@ -1,7 +1,23 @@
 /**
  * @author: Bhumika Ramesh
- *          All the features or functionality related to Account management
- *          would be available in this package
+ *          All the features or functionality related to Account management and
+ *          Customer Information
+ *          is available in this package
+ *          <b>Milestone 2 - Persistence:
+ *          <ul>
+ *          <li>Created Account.java, AccountRepository.java, Customer.java and
+ *          CustomerRepository.java classes.
+ *          <li>Defined bi-directional relationship between Account and Customer
+ *          and CustomerOrder classes.
+ *          <li>Added comments or documentations for newly added features in
+ *          Account and Customer classes that were not covered in class.
+ *          <li>New features used: Product class used nullable and Customer
+ *          class used cascade and orphanRemoval
+ *          <li>Updated data.sql to insert seed data into Account and Customer
+ *          tables
+ *          <li>Added unit tests for AccountRepository and CustomerRepository to
+ *          demonstrate that both of them are working
+ *          </ul>
  */
 
 package edu.depaul.cdm.se452.groupProject.account;
